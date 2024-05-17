3d7357f8de65fedce6f5b93a37b44816abb28dce
Ridouane GHERMI <ridouaneg@gmail.com>
2024-05-17 17:30:12 +0200

    Added SFD data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import csv
 import re
-import random
+import numpy as np
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Needed to use sessions
@@ -17,20 +17,20 @@ def read_csv(file_path):
             data.append(row)
     return data
 
-def save_answer(pseudo, movie_id, question_id, selected_option):
+def save_answer(pseudo, video_id, question_id, selected_option):
     with open('answers.csv', 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([pseudo, movie_id, question_id, selected_option])
+        writer.writerow([pseudo, video_id, question_id, selected_option])
 
 def get_unique_movies(movies):
     unique_movies = {}
     for movie in movies:
-        if movie['movie_id'] not in unique_movies:
-            unique_movies[movie['movie_id']] = movie
+        if movie['video_id'] not in unique_movies:
+            unique_movies[movie['video_id']] = movie
     return list(unique_movies.values())
 
-movies = read_csv('movies.csv')
-unique_movies = get_unique_movies(movies)
+movies = read_csv('data.csv')
+#unique_movies = get_unique_movies(movies)
 
 @app.route('/', methods=['GET', 'POST'])
 def pseudo():
@@ -45,16 +45,16 @@ def index():
     if 'pseudo' not in session:
         return redirect(url_for('pseudo'))
     #return render_template('index.html', movies=unique_movies)
-    random_movies = random.sample(movies, 8)  # Select 8 random movies
+    random_movies = np.random.choice(movies, 8, replace=False).tolist()  # Select 8 random movies
     return render_template('index.html', movies=random_movies)
 
-
-@app.route('/quiz/<int:movie_id>', methods=['GET', 'POST'])
-def quiz(movie_id):
+@app.route('/quiz/<video_id>', methods=['GET', 'POST'])
+def quiz(video_id):
     if 'pseudo' not in session:
         return redirect(url_for('pseudo'))
 
-    questions = [movie for movie in movies if int(movie['movie_id']) == movie_id]
+    questions = [movie for movie in movies if movie['video_id'] == video_id]
+    movie_title = questions[0]['movie_title'] if questions else 'Unknown Title'
 
     if request.method == 'POST':
         pseudo = session['pseudo']
@@ -62,10 +62,10 @@ def quiz(movie_id):
             question_id = question['question_id']
             selected_option = request.form.get(f'option_{question_id}')
             if selected_option:
-                save_answer(pseudo, movie_id, question_id, selected_option)
+                save_answer(pseudo, video_id, question_id, selected_option)
         return redirect(url_for('index'))
 
-    return render_template('quiz.html', questions=questions)
+    return render_template('quiz.html', questions=questions, movie_title=movie_title)
 
 if __name__ == '__main__':
     app.run(debug=True)
